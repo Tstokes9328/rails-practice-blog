ccb40b07395a853f8c3cadad616078db1e8176c7
Tayte Stokes <taytestokes9328@gmail.com>
2019-05-12 13:29:37 -0600

    generated controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_12_191322) do
+ActiveRecord::Schema.define(version: 2019_05_12_191756) do
+
+  create_table "models", force: :cascade do |t|
+    t.string "User"
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
